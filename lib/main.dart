0aba1c7c0bce6082316a55df27e5e7efc32654e5
zesipho <zesiphoamandla@gmail.com>
2022-09-29 15:38:44 +0200

    versin1

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MaterialApp(
+  home:Scaffold(
+    appBar:AppBar(
+      title:Text('My App'),
+      backgroundColor: Colors.purple,
+      centerTitle:true,
+    ),
+    body: Center(
+      child: Image(
+        image: AssetImage('assets/z.jpg'),
+      ),
+    ),
+
+    floatingActionButton: FloatingActionButton(onPressed: (){},
+      child: Text('Click'),
+    ),
+  ),
+));
+
